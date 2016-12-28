3b6e49cf30786622a6ae93ae728d0c59d5b78039
sgaikwad <gaikwad.swapnil@gmail.com>
2016-12-28 23:51:44 +0000

    Create class for stack-vm

---
+++ stack-vm.cpp
@@ -0,0 +1,33 @@
+#ifndef STACK_VM_H
+#define STACK_VM_H
+
+#include <iostream>
+#include <vector>
+
+// type definitions
+typedef int32_t i32;
+
+class StackVM {
+    i32 pc = 100;	// Program counter
+    i32 sp = 0;		// Stack pointer
+    std::vector<i32> memory;
+    i32 typ = 0;
+    i32 dat= 0;
+    i32 running = 1;
+
+    // private functions
+    i32 getType(i32 instruction);
+    i32 getData(i32 instruction);
+    void fetch();
+    void decode();
+    void execute();
+    void doPrimitive();
+
+    public:
+    //public functions
+    StackVM();
+    void run();
+    void loadProgram(std::vector<i32> prog);
+};
+
+#endif

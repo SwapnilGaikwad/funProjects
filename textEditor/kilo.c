b9520e0993af4f9bef0c62e9728a779963e43d70
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-18 23:20:30 +0100

    Step 23: Reposition the cursor

--- textEditor/kilo.c
+++ textEditor/kilo.c
@@ -50,9 +50,11 @@ char editorReadKey(){
   }
   return c;
 }
-/*** ***/
+
+/*** output ***/
 void editorRefreshScreen(){
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** Input ***/

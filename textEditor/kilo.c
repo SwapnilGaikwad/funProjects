cea8f5e0381e502d8edbb0d0a64382590ef3860c
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-21 18:11:36 +0100

    Step 43-49: Move cursor on screen

--- textEditor/kilo.c
+++ textEditor/kilo.c
@@ -11,8 +11,16 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
 /*** data ***/
 struct editorConfig{
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -28,13 +36,31 @@ void die(const char *s) {
 }
 
 /*** terminal ***/
-char editorReadKey(){
+int editorReadKey(){
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if(nread == -1 && errno != EAGAIN) die("read");
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '['){
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 void disableRawMode() {
@@ -150,15 +176,44 @@ void editorRefreshScreen(){
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
-  abAppend(&ab, "\x1b[H", 3);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
 
-/*** Input ***/
+/*** input ***/
+void editorMoveCursor(int key) {
+  switch(key) {
+    case ARROW_LEFT:
+         if (E.cx != 0){
+           E.cx--;
+         }
+         break;
+    case ARROW_RIGHT:
+         if (E.cx != E.screencols - 1){
+           E.cx++;
+         }
+         break;
+    case ARROW_UP:
+         if (E.cy != 0){
+           E.cy--;
+         }
+         break;
+    case ARROW_DOWN:
+         if (E.cy != E.screenrows - 1){
+           E.cy++;
+         }
+         break;
+  }
+}
+
 void editorProcessKeypress(){
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch(c){
     case CTRL_KEY('q'):
@@ -166,11 +221,19 @@ void editorProcessKeypress(){
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+   case ARROW_UP:
+   case ARROW_DOWN:
+   case ARROW_LEFT:
+   case ARROW_RIGHT:
+      editorMoveCursor(c);
+      break;
   }
 }
  
 /*** init ***/
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
   if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 

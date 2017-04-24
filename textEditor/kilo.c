21a1a84b01c6c86fb382f5f5731ccd2ce013d026
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-04-24 22:38:53 +0100

    Step 56-57: Show line on console

--- textEditor/kilo.c
+++ textEditor/kilo.c
@@ -5,6 +5,7 @@
 #include<ctype.h>
 #include<errno.h>
 #include<sys/ioctl.h>
+#include<sys/types.h>
 #include<string.h>
 
 /*** defines***/
@@ -154,6 +155,19 @@ int getWindowSize(int *rows, int *cols){
     return 0;
   }
 }
+
+/*** file i/o ***/
+void editorOpen() {
+  char *line = "Hello, world!";
+  ssize_t linelen = 13;
+
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
+}
+
 /*** append buffer ***/
 struct abuf {
   char *b;
@@ -179,23 +193,29 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
   int y;
   for(y=0; y<E.screenrows; y++){
-    if(y == E.screenrows/3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-                       "Kilo editor -- version %s", KILO_VERSION);
-      if(welcomelen > E.screencols) {
-        welcomelen = E.screencols;
-      }
-      int padding = (E.screencols - welcomelen)/2;
-      if (padding){
+    if(y >= E.numrows) {
+      if(y == E.screenrows/3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+                         "Kilo editor -- version %s", KILO_VERSION);
+        if(welcomelen > E.screencols) {
+          welcomelen = E.screencols;
+        }
+        int padding = (E.screencols - welcomelen)/2;
+        if (padding){
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
-    }
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row.chars, len);
+    } 
     abAppend(ab, "\x1b[K", 3);
     if(y < E.screenrows-1) {
       abAppend(ab, "\r\n", 2);
@@ -294,6 +314,7 @@ void initEditor() {
 int main() {
   enableRawMode();
   initEditor();
+  editorOpen();
 
   while(1){
     editorRefreshScreen();
